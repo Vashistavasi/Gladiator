e69dc6202ad7a9e7576bdf2f6dc8fa1fd13c916f
akashpatel <ap1706027@gmail.com>
2021-08-11 08:29:02 +0530

    Add toString and constructor in AdminInfo.java beans

--- OnlineExaminationApp/src/main/java/com/lti/beans/AdminInfo.java
+++ OnlineExaminationApp/src/main/java/com/lti/beans/AdminInfo.java
@@ -12,12 +12,28 @@ public class AdminInfo {
 	@Id
 	@GeneratedValue
 	private int admin_id;
+	
 	@Column(nullable = false)
     private String name; //varchar2(50) NOT NULL,
+	
 	@JoinColumn
     @OneToOne
     private LoginDetails  email_id; //varchar2(100) NOT NULL UNIQUE,
-    private int mobile_no; //NUMBER(10) NOT NULL,
+	
+    @Override
+	public String toString() {
+		return "AdminInfo [admin_id=" + admin_id + ", name=" + name + ", email_id=" + email_id + ", mobile_no="
+				+ mobile_no + "]";
+	}
+	public AdminInfo(int admin_id, String name, LoginDetails email_id, int mobile_no) {
+		super();
+		this.admin_id = admin_id;
+		this.name = name;
+		this.email_id = email_id;
+		this.mobile_no = mobile_no;
+	}
+	private int mobile_no; //NUMBER(10) NOT NULL,
+    
     public int getAdmin_id() {
 		return admin_id;
 	}
